4c758818ccbf45eb9c6fa4bfbdd0e439feda2b67
spcre <spcrey@outlook.com>
2024-10-21 20:03:02 +0800

    add sqlite

--- src/MainKotlin.kt
+++ src/MainKotlin.kt
@@ -1,13 +1,25 @@
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.ResultSet
+import java.sql.Statement
+
 object MainKotlin {
     @JvmStatic
     fun main(args:Array<String>) {
-        val solution = Solution()
-        val n = readln().toInt()
-        val array = readln().split(" ").map{ it.toInt() }
-        println(array)
+        val dbFile = "D:\\Main Files\\Projects\\learn-underlying\\resource\\database.db"
+        Class.forName("org.sqlite.JDBC")
+        val connection: Connection = DriverManager.getConnection("jdbc:sqlite:$dbFile")
+        val statement: Statement = connection.createStatement()
+        val resultSet: ResultSet = statement.executeQuery(
+            "SELECT * FROM Student;"
+        )
+        while (resultSet.next()) {
+            val name = resultSet.getString("name")
+            println("name: $name")
+        }
+        resultSet.close()
+        statement.close()
+        connection.close()
     }
 }
 
-class Solution {
-
-}
